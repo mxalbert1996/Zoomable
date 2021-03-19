d57c26d068653bafac73fa46865382a87ac07acd
Tlaster <tlaster@outlook.com>
2021-03-19 19:20:40 +0800

    init

---
+++ zoomable/src/main/java/moe/tlaster/zoomable/Zoomable.kt
@@ -0,0 +1,110 @@
+@file:JvmName("Zoomable")
+
+package moe.tlaster.zoomable
+
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.consumePositionChange
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.layout
+import kotlinx.coroutines.launch
+
+/**
+ * A zoomable layout that can handle zoom in and out with drag support.
+ *
+ * @param state the state object to be used to observe the [Zoomable] state.
+ * @param modifier the modifier to apply to this layout.
+ * @param content a block which describes the content.
+ */
+@Composable
+fun Zoomable(
+    state: ZoomableState,
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+    BoxWithConstraints(
+        modifier = modifier,
+    ) {
+        var childWidth by remember { mutableStateOf(0) }
+        var childHeight by remember { mutableStateOf(0) }
+        LaunchedEffect(
+            childHeight,
+            childWidth,
+            state.scale,
+        ) {
+            val maxX = (childWidth * state.scale - constraints.maxWidth)
+                .coerceAtLeast(0F) / 2F
+            val maxY = (childHeight * state.scale - constraints.maxHeight)
+                .coerceAtLeast(0F) / 2F
+            state.updateBounds(maxX, maxY)
+        }
+        val transformableState = rememberTransformableState { zoomChange, _, _ ->
+            state.onZoomChange(zoomChange)
+        }
+        Box(
+            modifier = Modifier
+                .pointerInput(Unit) {
+                    detectDragGestures(
+                        onDrag = { change, dragAmount ->
+                            if (state.zooming) {
+                                change.consumePositionChange()
+                                scope.launch {
+                                    state.drag(dragAmount)
+                                    state.addPosition(
+                                        change.uptimeMillis,
+                                        change.position
+                                    )
+                                }
+                            }
+                        },
+                        onDragCancel = {
+                            state.resetTracking()
+                        },
+                        onDragEnd = {
+                            if (state.zooming) {
+                                scope.launch {
+                                    state.dragEnd()
+                                }
+                            }
+                        },
+                    )
+                }
+                .transformable(state = transformableState)
+                .layout { measurable, constraints ->
+                    val placeable =
+                        measurable.measure(constraints = constraints)
+                    childHeight = placeable.height
+                    childWidth = placeable.width
+                    layout(
+                        width = constraints.maxWidth,
+                        height = constraints.maxHeight
+                    ) {
+                        placeable.placeRelativeWithLayer(
+                            (constraints.maxWidth - placeable.width) / 2,
+                            (constraints.maxHeight - placeable.height) / 2
+                        ) {
+                            scaleX = state.scale
+                            scaleY = state.scale
+                            translationX = state.translateX
+                            translationY = state.translateY
+                        }
+                    }
+                }
+        ) {
+            content.invoke(this)
+        }
+    }
+}
