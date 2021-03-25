78cc571a5440061fe596d23be40246580676af94
Tlaster <tlaster@outlook.com>
2021-03-25 10:50:52 +0800

    sample with pager

--- sample/src/main/java/moe/tlaster/zoomable/sample/MainActivity.kt
+++ sample/src/main/java/moe/tlaster/zoomable/sample/MainActivity.kt
@@ -3,8 +3,13 @@ package moe.tlaster.zoomable.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import moe.tlaster.zoomable.Zoomable
 import moe.tlaster.zoomable.rememberZoomableState
 
@@ -17,12 +22,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 private fun Sample() {
-    val state = rememberZoomableState(
-        minScale = 2f
-    )
-    Zoomable(state = state) {
-        Text(text = "Zoom me!")
+    val pagerState = rememberPagerState(pageCount = 10)
+    HorizontalPager(state = pagerState) { page ->
+        val state = rememberZoomableState(
+            minScale = 2f
+        )
+        Zoomable(
+            state = state,
+        ) {
+            // Our page content
+            Text(
+                text = "Page: $page",
+            )
+        }
     }
 }
