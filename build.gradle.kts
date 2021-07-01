8a778f78637c1e8b71956090775a15821ad5cd91
Albert Chang <mxalbert1996@gmail.com>
2021-07-01 12:31:48 +0900

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-beta03")
+        classpath("com.android.tools.build:gradle:7.0.0-beta04")
         classpath(kotlin("gradle-plugin", "1.5.10"))
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.15.1")
     }
