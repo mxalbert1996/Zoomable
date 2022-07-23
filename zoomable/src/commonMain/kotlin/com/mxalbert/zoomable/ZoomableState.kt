83709fa1452508be0ab4b3a8cdeb2d2a514c8c96
Albert Chang <mxalbert1996@gmail.com>
2022-07-24 00:25:39 +0900

    Add comment

--- zoomable/src/commonMain/kotlin/com/mxalbert/zoomable/ZoomableState.kt
+++ zoomable/src/commonMain/kotlin/com/mxalbert/zoomable/ZoomableState.kt
@@ -189,6 +189,9 @@ class ZoomableState(
         }
 
     private fun updateBounds() {
+        // If the child is an asynchronously loaded image, its size will be temporarily zero after
+        // the translation values are restored. In that case we don't update the bounds to avoid
+        // resetting the translation values.
         if (childSize == Size.Zero) return
         val offsetX = childSize.width * scale - size.width
         val offsetY = childSize.height * scale - size.height
