61bbbfb42b12d5b4b02a061397e3806c50cbb785
Albert Chang <mxalbert1996@gmail.com>
2023-04-20 23:26:23 +0900

    Improve build script (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.android.build.api.dsl.CommonExtension as AndroidCommonExtension
-
 buildscript {
     repositories {
         google()
@@ -9,28 +6,6 @@ buildscript {
     dependencies {
         classpath(libs.agp)
         classpath(libs.kotlin.gradle)
-    }
-}
-
-subprojects {
-    afterEvaluate {
-        extensions.findByType(AndroidCommonExtension::class)?.apply {
-            buildFeatures {
-                buildConfig = false
-            }
-
-            compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_1_8
-                targetCompatibility = JavaVersion.VERSION_1_8
-            }
-        }
-
-        tasks.withType<KotlinCompile> {
-            kotlinOptions {
-                jvmTarget = JavaVersion.VERSION_1_8.toString()
-                freeCompilerArgs = freeCompilerArgs + "-opt-in=kotlin.RequiresOptIn"
-            }
-        }
     }
 }
 
