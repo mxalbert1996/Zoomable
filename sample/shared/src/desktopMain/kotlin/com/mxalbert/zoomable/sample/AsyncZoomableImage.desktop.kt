ce86dd263e89a1d35890e7521050a844359f9a0e
Albert Chang <mxalbert1996@gmail.com>
2025-03-14 23:34:42 +0900

    Update sample to use Coil 3

--- sample/shared/src/desktopMain/kotlin/com/mxalbert/zoomable/sample/AsyncZoomableImage.desktop.kt
+++
@@ -1,6 +0,0 @@
-package com.mxalbert.zoomable.sample
-
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.java.Java
-
-internal actual val httpClient = HttpClient(Java)
--- sample/shared/src/iosMain/kotlin/com/mxalbert/zoomable/sample/main.ios.kt
+++ sample/shared/src/iosMain/kotlin/com/mxalbert/zoomable/sample/main.ios.kt
@@ -1,27 +1,7 @@
 package com.mxalbert.zoomable.sample
 
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
-import kotlinx.cinterop.ExperimentalForeignApi
-import kotlinx.cinterop.useContents
-import platform.UIKit.UIApplication
-import platform.UIKit.UIEdgeInsets
 
-@OptIn(ExperimentalForeignApi::class)
 fun MainViewController() = ComposeUIViewController {
-    val insets = UIApplication.sharedApplication.keyWindow
-        ?.safeAreaInsets?.useContents { toWindowInsets() }
-        ?: LocalWindowInsets.current
-    CompositionLocalProvider(LocalWindowInsets provides insets) {
-        ZoomableApp()
-    }
+    ZoomableAppWithPager()
 }
-
-private fun UIEdgeInsets.toWindowInsets(): WindowInsets = WindowInsets(
-    left = left.dp,
-    top = top.dp,
-    right = right.dp,
-    bottom = bottom.dp
-)
